e54e93434e4660cdb4d3e3926c2294c6d696b84a
chessai <chessai1996@gmail.com>
2019-07-09 10:39:17 -0400

    initial api

--- src/Data/Primitive/Pinned.hs
+++ src/Data/Primitive/Pinned.hs
@@ -8,19 +8,46 @@
       , RoleAnnotations
       , ScopedTypeVariables
       , StandaloneDeriving
+      , TypeApplications
   #-}
 
 module Data.Primitive.Pinned
-  ( Pinnedness(..)
+  (
+    -- * Types
+    Pinnedness(..)
   , ByteArray(..)
   , MutableByteArray(..)
-  , new, newPinned
+
+    -- * Creation
+  , new
+  , newPinned
+
+    -- * Querying
+  , size
+  , sizeMutable
+
+    -- * Conversion
+  , unpinnedToPinned
+  , pinnedToUnpinned
+  , unsafeFreeze
+
+    -- ** Conversion to/from 'Data.ByteString.ByteString'
+  , pinnedToByteString
+  , byteStringToPinned
   ) where
 
 import GHC.Exts
+import GHC.ST
+import GHC.ForeignPtr
+import Foreign.ForeignPtr
+import GHC.Ptr (plusPtr)
+import Data.Word (Word8)
+import Data.Primitive.Foreign (peekArray)
 import Control.Monad.Primitive (PrimMonad(..))
+import Data.ByteString.Internal
 
 import qualified Data.Primitive.ByteArray as P
+import qualified Data.Primitive.PrimArray as P
 
 data Pinnedness = Pinned | Unpinned
 
@@ -36,18 +63,69 @@ type role MutableByteArray nominal nominal
 new :: PrimMonad m
   => Int
   -> m (MutableByteArray 'Unpinned (PrimState m))
-new !sz = do { marr <- P.newByteArray sz; pure (MutableByteArray marr) }
+new !sz = do
+  marr <- P.newByteArray sz
+  pure (MutableByteArray marr)
+{-# inline new #-}
 
 newPinned :: PrimMonad m
   => Int
   -> m (MutableByteArray 'Pinned (PrimState m))
-newPinned !sz = do { marr <- P.newPinnedByteArray sz; pure (MutableByteArray marr) }
+newPinned !sz = do
+  marr <- P.newPinnedByteArray sz
+  pure (MutableByteArray marr)
+{-# inline newPinned #-}
 
-{-
-byteArrayToByteString :: ByteArray 'Pinned -> ByteString
-byteArrayToByteString (ByteArray (P.ByteArray b#)) =
+unsafeFreeze :: PrimMonad m
+  => MutableByteArray p (PrimState m)
+  -> m (ByteArray p)
+unsafeFreeze (MutableByteArray marr) = ByteArray
+  <$> P.unsafeFreezeByteArray marr
+{-# inline unsafeFreeze #-}
+
+size :: ByteArray p -> Int
+size (ByteArray arr) = P.sizeofByteArray arr
+{-# inline size #-}
+
+sizeMutable :: PrimMonad m => MutableByteArray p (PrimState m) -> m Int
+sizeMutable (MutableByteArray marr) = P.getSizeofMutableByteArray marr
+{-# inline sizeMutable #-}
+
+-- | Copy an 'Unpinned' 'Bytearray' into a 'Pinned' 'ByteArray'.
+unpinnedToPinned :: ()
+  => ByteArray 'Unpinned
+  -> ByteArray 'Pinned
+unpinnedToPinned b@(ByteArray unpinned) = ByteArray $ runST $ do
+  let !sz = size b
+  marr <- P.newPinnedByteArray sz
+  P.copyByteArray marr 0 unpinned 0 sz
+  P.unsafeFreezeByteArray marr
+{-# inline unpinnedToPinned #-}
+
+-- | Copy a 'Pinned' 'ByteArray' into an 'Unpinned' 'ByteArray'.
+pinnedToUnpinned :: ()
+  => ByteArray 'Pinned
+  -> ByteArray 'Unpinned
+pinnedToUnpinned b@(ByteArray pinned) = ByteArray $ runST $ do
+  let !sz = size b
+  marr <- P.newByteArray sz
+  P.copyByteArray marr 0 pinned 0 sz
+  P.unsafeFreezeByteArray marr
+{-# inline pinnedToUnpinned #-}
+
+pinnedToByteString :: ByteArray 'Pinned -> ByteString
+pinnedToByteString (ByteArray (P.ByteArray b#)) =
   let contents# = byteArrayContents# b#
       fp = ForeignPtr contents# (PlainPtr (unsafeCoerce# b#))
       len = I# (sizeofByteArray# b#)
   in PS fp 0 len
--}
+{-# inline pinnedToByteString #-}
+
+byteStringToPinned :: ByteString -> ByteArray 'Pinned
+byteStringToPinned (PS fp off len) = accursedUnutterablePerformIO $
+  withForeignPtr fp $ \ptr -> do
+    (P.PrimArray bytearray#) <- peekArray @Word8 len (plusPtr ptr off)
+    let b = ByteArray (P.ByteArray bytearray#)
+    pure (unpinnedToPinned b)
+{-# inline byteStringToPinned #-}
+
